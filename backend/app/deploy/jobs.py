89c213ced9cffca3c857323fe1ff60445001bb24
Ethkuil <hobartlin23@gmail.com>
2022-08-26 16:33:17 +0800

    feat(backend): 取消测试用的额外模拟延时

--- backend/app/deploy/jobs.py
+++ backend/app/deploy/jobs.py
@@ -1,17 +1,8 @@
-import imp
-
-import random
-import time
-
 from app.data import JOBS, MODELS
 from app.utils.predict import predictDataset
 
 
 def deployJob(jobId, datasetFilePath):
-    # 启动中
-    # 用随机sleep时间，模拟启动时间
-    time.sleep(random.uniform(1, 5))
-
     # 运行中
     JOBS.updateJob(jobId, state='运行中')
     model = MODELS.getModel(JOBS.getJob(jobId).modelId)
--- backend/app/deploy/services.py
+++ backend/app/deploy/services.py
@@ -1,14 +1,7 @@
-import random
-import time
-
 from app.data import SERVICES, MODELS
 from app.utils.predict import predict
 
-def deployService(serviceId, modelPath):
-    # 启动中
-    # 用随机sleep时间，模拟启动时间
-    time.sleep(random.uniform(1, 5))
-
+def deployService(serviceId, modelId):
     # 运行中
     SERVICES.updateService(serviceId, state='运行中')
 
